01117f9fc8d9da8e1caa902b943de02d76204e2a
pgkwon1@naver.com <pgkwon1@naver.com>
2024-04-11 21:15:02 +0900

    update: es6 방식 수정 및 서버 시작 추가.

--- app.js
+++ app.js
@@ -1,23 +1,20 @@
-const createError = require("http-errors");
-const express = require("express");
-const path = require("path");
-const cookieParser = require("cookie-parser");
-
-const indexRouter = require("./routes/index");
-
+import createError from "http-errors";
+import express from "express";
+import path from "path";
+import indexRouter from "./routes/index.js";
+import cookieParser from "cookie-parser";
 const app = express();
 
-app.use(logger("dev"));
+//app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(import.meta.url, "public")));
 
 app.use("/", indexRouter);
 
-// catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
 });
 
-module.exports = app;
+app.listen(3000, async () => {});
