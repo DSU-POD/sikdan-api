4fa591720a239eaf1c6598bc7f9e59f83a5221ff
pgkwon1 <pgkwon1@naver.com>
2024-07-07 17:22:59 +0900

    update: member.js

--- routes/member.js
+++ routes/member.js
@@ -1,7 +1,7 @@
 import express from "express";
 const router = express.Router();
 
-router.get("/login", function (req, res, next) {
+router.post("/login", function (req, res, next) {
   res.send("test");
 });
 
