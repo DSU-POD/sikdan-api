225f5240ab364f3f44d0d8e5baaa4630062b4ef1
Youmin02 <leeyumin080@gmail.com>
2024-07-18 18:38:59 +0900

    update: status 이슈 해결

--- routes/member.js
+++ routes/member.js
@@ -6,27 +6,27 @@ const memberService = new MemberService();
 router.post("/login", async function (req, res, next) {
   try {
     const result = await memberService.login();
-    res.statusCode(200).send("로그인 성공 하였습니다.");
+    res.status(200).send("로그인 성공 하였습니다.");
   } catch (e) {
-    res.statusCode(400).send("로그인 실패 하였습니다.");
+    res.status(400).send("로그인 실패 하였습니다.");
   }
 });
 
 router.get("/find_id", async function (req, res, next) {
   try {
     const result = await memberService.findId();
-    res.statusCode(200).send("아이디는" + userId + "입니다.");
+    res.status(200).send("아이디는" + userId + "입니다.");
   } catch (e) {
-    res.statusCode(400).send("해당 이메일로 등록된 계정이 없습니다.");
+    res.status(400).send("해당 이메일로 등록된 계정이 없습니다.");
   }
 });
 
 router.get("/find_password", async function (req, res, next) {
   try {
     const result = await memberService.findPassword();
-    res.statusCode(200).send("비밀번호는" + password + "입니다.");
+    res.status(200).send("비밀번호는" + password + "입니다.");
   } catch (e) {
-    res.statusCode(400).send("해당 입력정보로 등록된 계정이 없습니다.");
+    res.status(400).send("해당 입력정보로 등록된 계정이 없습니다.");
   }
 });
 
