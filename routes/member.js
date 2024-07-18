d4a8fe0ad5e455183d2fb1855d0d1fda75804cc2
Youmin02 <leeyumin080@gmail.com>
2024-07-18 16:19:38 +0900

    Update: login

--- routes/member.js
+++ routes/member.js
@@ -1,8 +1,27 @@
 import express from "express";
+import { MemberService } from "../services/member/member.service";
 const router = express.Router();
+const memberService = new MemberService();
 
-router.post("/login", function (req, res, next) {
-  res.send("test");
+router.post("/login", async function (req, res, next) {
+  
+  try{
+    const result = await memberService.login();
+    res.state(200).send("로그인 성공 하였습니다.");
+  }catch(e){
+    res.state(400).send("로그인 정보가 없습니다.");
+  }
+  
+});
+
+router.get("/find_id", async function (req, res, next) {
+  
+  
+});
+
+router.get("/find_password", async function (req, res, next) {
+  
+  
 });
 
 export default router;
