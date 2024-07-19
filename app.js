d14fa40aa680c8781523e70f4231949e3ab115d8
pgkwon1@naver.com <pgkwon1@naver.com>
2024-07-19 19:10:41 +0900

    update: body-parser, jwt 미들웨어 관련 개선

--- app.js
+++ app.js
@@ -3,28 +3,37 @@ import path from "path";
 import indexRouter from "./routes/index.js";
 import cookieParser from "cookie-parser";
 import JwtStrateGy from "./auth/jwt.strategy.js";
+import bodyParser from "body-parser";
+
 const app = express();
 
 //app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(express.static(path.join(import.meta.url, "public")));
 
 // jwt middleware
 app.use(async (req, res, next) => {
   try {
-    if (req.headers.authrozation === "") {
-      throw new Error("비정상적인 접근입니다.");
-    }
     if (req.method !== "GET" && req.path !== "/member/login") {
+      if (req.headers.authrozation === "") {
+        throw new Error("비정상적인 접근입니다.");
+      }
       const token = req.headers.authorization.split(" ")[1];
       JwtStrateGy.validateJwt(token);
     }
-    return true;
   } catch (e) {
     next(e);
   }
+
+  next();
 });
 app.use("/", indexRouter);
 
