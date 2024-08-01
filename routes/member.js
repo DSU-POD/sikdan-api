6dd47fb6680d8ef6f10dc31389282defa716d32d
Youmin02 <leeyumin080@gmail.com>
2024-08-01 18:53:18 +0900

    feat: 응답 개선

--- routes/member.js
+++ routes/member.js
@@ -23,7 +23,10 @@ router.post("/find_id", async function (req, res, next) {
   try {
     const { email } = req.body;
     await memberService.findId(email);
-    next("회원정보에 입력한 이메일로 아이디를 발송해드렸습니다.");
+    next({
+      data: "",
+      message: "회원정보에 입력한 이메일로 아이디를 발송해드렸습니다.",
+    });
   } catch (e) {
     next(e);
   }
@@ -34,7 +37,10 @@ router.post("/find_password", async function (req, res, next) {
     if (req.body.userId && req.body.email) {
       const { userId, email } = req.body;
       await memberService.findPassword(userId, email);
-      next("회원정보에 입력한 이메일로 임시 비밀번호를 발송해드렸습니다.");
+      next({
+        data: "",
+        message: "회원정보에 입력한 이메일로 임시 비밀번호를 발송해드렸습니다.",
+      });
     } else {
       throw new Error();
     }
@@ -48,7 +54,10 @@ router.post("/register/complete", async (req, res, next) => {
     const { registerData } = req.body;
 
     await memberService.register(registerData);
-    next("회원가입 되었습니다.");
+    next({
+      data: "",
+      message: "회원가입 되었습니다.",
+    });
   } catch (e) {
     next(e);
   }
