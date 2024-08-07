e2b8f5adad9d64a450764ed34faa9292ce9c8d2a
Youmin02 <leeyumin080@gmail.com>
2024-08-07 23:02:35 +0900

    update: delete/:id 라우터 생성

--- routes/feed.js
+++ routes/feed.js
@@ -1,6 +1,7 @@
 import express from "express";
-import FeedService from "../services/feed/feed.service";
+import FeedService from "../services/feed/feed.service.js";
 const router = express.Router();
+const feedService = new FeedService();
 
 router.post("/like", async (req, res, next) => {});
 router.delete("/likeCancel", async (req, res, next) => {});
@@ -8,17 +9,28 @@ router.delete("/likeCancel", async (req, res, next) => {});
 router.get("/list", async (req, res, next) => {});
 router.get("/view/:id", async (req, res, next) => {
   try {
-    const { feedId } = req.body;
-    const post = await FeedService.feedPost(feedId);
-    if (!post) {
-      return res.status(404).send({ message: "404 err: 게시물을 찾을 수 없습니다." });
-    }
-    res.send(post);
+    const { id } = req.params;
+    const post = await feedService.getFeed(id);
+    next({
+      data: post,
+      message: "조회 되었습니다.",
+    });
   } catch (e) {
     next(e);
   }
 });
 router.patch("/edit/:id", async (req, res, next) => {});
-router.delete("/delete/:id", async (req, res, next) => {});
+router.delete("/delete/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const post = await feedService.deleteFeed(id);
+    next({
+      data: post,
+      message: "삭제 되었습니다.",
+    });
+  } catch (e) {
+    next(e);
+  }
+});
 
 export default router;
