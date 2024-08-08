9d8d8cb084a53fe7324fd09e6414111c6ba86822
pgkwon1@naver.com <pgkwon1@naver.com>
2024-08-08 12:47:11 +0900

    feat: feed list 조회기능

--- services/feed/feed.service.js
+++ services/feed/feed.service.js
@@ -32,6 +32,45 @@ export default class FeedService {
     return feedInfo;
   }
 
+  async getFeedList(page, type) {
+    let offset = 0;
+    if (page > 1) {
+      offset = 10 * (page - 1);
+    }
+    // 피드 아이디가 있는지 확인
+    const feedInfo = await this.FeedModel.findAll({
+      limit: 10,
+      offset,
+      where: {
+        type,
+      },
+      attributes: [
+        "id",
+        "subject",
+        "contents",
+        "ai_feedback",
+        "likeNum",
+        "commentNum",
+      ],
+      include: [
+        {
+          model: this.LikeModel,
+          as: "feedLike",
+          attributes: ["memberId"],
+        },
+        {
+          model: this.MemberModel,
+          as: "memberFeed",
+          attributes: ["userId", "nickname"],
+        },
+      ],
+    });
+    if (feedInfo === null) {
+      throw new Error("404 err");
+    }
+    return feedInfo;
+  }
+
   async deleteFeed(id) {
     const feedInfo = await this.FeedModel.findOne({
       where: {
