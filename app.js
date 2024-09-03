48c9b9b7e137f9d13be0455bd858bbbf71c14121
pgkwon1@naver.com <pgkwon1@naver.com>
2024-09-03 19:32:34 +0900

    update: jwt 예외 경로 추가.

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ app.use(async (req, res, next) => {
       "/member/find_id",
       "/member/find_password",
       "/member/register/complete",
+      "/member/register/duplicate",
     ];
     if (!exceptPath.includes(req.path)) {
       if (
