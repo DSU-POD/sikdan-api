891dd836e4576305647352580d225c3598657eea
pgkwon1@naver.com <pgkwon1@naver.com>
2024-09-03 19:33:00 +0900

    update: 좋아요 기능 개선

--- routes/feed.js
+++ routes/feed.js
@@ -56,11 +56,10 @@ router.post("/predict", upload.single("file"), async (req, res, next) => {
 router.post("/like", async (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
-    const decoded = JwtStrateGy.validateJwt(token);
-    const memberId = decoded.userId;
-
+    const decoded = await JwtStrateGy.validateJwt(token);
+    const memberId = decoded.memberId;
     const { feedId } = req.body;
-    if (!memberId || !req.body.feedId) {
+    if (!memberId || !feedId) {
       // 회원 또는 피드 없으면 퇴각
       throw new Error("비정상적인 접근입니다.");
     }
